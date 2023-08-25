8b18efbad612dde56c651ce02b0335f2a57ddea0
rpetr127 <nalrom4ever@gmail.com>
2023-08-25 15:32:54 +0300

    Minor fixes and improvements

--- assets/item_event.js
+++ assets/item_event.js
@@ -1,11 +1,10 @@
 const feeds = [];
+const item = document.getElementsByClassName('item')
+item[1].addEventListener("onclick", addItemToList)
 
-(function () {
-  const item = document.getElementsByClassName('item')
-  item[1].Onlick(function () {
-    console.log(item[0].textContent)
-    const ind = feeds.length
-    feeds[ind] = item[0].textContent
-  })
+function addItemToList() {
+  console.log(item[0].textContent)
+  const ind = feeds.length
+  feeds[ind] = item[0].textContent
   return feeds
-})()
+}
