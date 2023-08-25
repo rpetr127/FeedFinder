7e24327756b89b0dc0f4159d2e5a383dbbd4f100
rpetr127 <62595366+rpetr127@users.noreply.github.com>
2023-08-25 10:42:08 +0000

    Minor fixes and improvements

--- assets/main.js
+++ assets/main.js
@@ -1,32 +1,36 @@
 import feeds from './item_event';
 
 (function () {
-  const refList = document.querySelector('p').querySelectorAll('a')
-  const myInput = document.querySelector('input')
+  const refList = document.querySelector('p').querySelectorAll('a');
+  const myInput = document.querySelector('input');
 
   for (let i = 0; i <= refList.length; i++) {
     refList[i].addEventListener('click', function (event) {
-      event.preventDefault()
-      myInput.value = event.target.text
+      event.preventDefault();
+      myInput.value = event.target.text;
     })
   }
-})()
+})();
 
-const WebApp = window.Telegram.WebApp
+var WebApp = window.Telegram.WebApp;
 
-const MainButton = WebApp.MainButton
-const BackButton = WebApp.BackButton
+var MainButton = WebApp.MainButton;
+var BackButton = WebApp.BackButton;
 
-MainButton.show()
-BackButton.show()
+if !MainButton.isVisible {
+  MainButton.show();
+}
+if !BackBtton.isVisible {
+  BackButton.show();
+}
 
 MainButton.onClick(function () {
-  const xhrURL = new URL('https://188.225.82.102:8443')
+  const xhrURL = new URL('https://188.225.82.102:8443');
   for (let i = 0; i <= feeds.length; i++) {
-    xhrURL.searchParams.set('feed', feeds[i])
-    const xhr = new XMLHttpRequest()
-    xhr.open('GET', xhrURL)
-    xhr.send()
-    WebApp.sendData(feeds[i])
+    xhrURL.searchParams.set('feed', feeds[i]);
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', xhrURL);
+    xhr.send();
+    WebApp.sendData(feeds[i]);
   }
-})
+});
