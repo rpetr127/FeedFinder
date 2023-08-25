b2c9ba2cc1dfdd9e1905d1c92e16090518abe0d3
rpetr127 <nalrom4ever@gmail.com>
2023-08-25 14:27:21 +0300

    Minor fixes and improvements

--- assets/item_event.js
+++ assets/item_event.js
@@ -3,8 +3,9 @@ const feeds = [];
 (function () {
   const item = document.getElementsByClassName('item')
   item[1].Onlick(function () {
+    console.log(item[0].textContent)
     const ind = feeds.length
-    feeds[ind] = item
+    feeds[ind] = item[0].textContent
   })
   return feeds
 })()
